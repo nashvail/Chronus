e91665377c16b5ddc7e466be6128306dff6ba102
Nash Vail <nishantve1@gmail.com>
2015-01-26 18:38:02 +0530

    Initial commit of version one of working extenstion

---
+++ js/background.js
@@ -0,0 +1,245 @@
+// Global variable to store the active tab url
+var activeTabUrl = "";
+
+// Global variable that stores the amount of time spent by the user on facebook.com and twitter.com
+var totalTimeOnWebsites;
+
+// Global variable to track whether the user is active or not
+var isUserActive;
+
+// Stores the current date or the days that have passed since the UTC till today
+var today;
+
+// Stores the name of the key that stores the data for the current date 
+var todayStorageName;
+
+// An array of strings that hold the domain names of the websites that are supposed to be checked
+var websitesToTrack;
+
+var isFirstRun = false;
+
+// Returns the current website being used
+function getActiveWebsite() {
+    return extractDomain(activeTabUrl);
+}
+
+// Returns whether the user is active right now or not
+function isUserActiveNow() {
+    return isUserActive;
+}
+
+// Will check if this is the first time the extension has been run, if yes then sets the timeSpentOnWebsites to 0
+chrome.runtime.onInstalled.addListener(function(details){
+    if(details.reason == "install"){
+        isFirstRun = true;
+        // Initialize the data objec that is to be placed in the localStorage
+        var data = {};
+
+        // Assign initial values to the variables
+        today = numDaysSinceUTC();
+        todayStorageName = "timeData" + numDaysSinceUTC();
+        totalTimeOnWebsites = 0;
+        websitesToTrack = ["twitter.com", "facebook.com", "plus.google.com"];
+
+        // Store the values in the localStorage
+        data[todayStorageName] = totalTimeOnWebsites;
+        data["today"] = numDaysSinceUTC();
+        data["trackData"] = JSON.stringify(websitesToTrack);
+        chrome.storage.local.set(data, function(){});
+    }
+});
+
+// Detect for change in storage and update the variables
+chrome.storage.onChanged.addListener(function(){
+    // update the websites to track variable
+    chrome.storage.local.get("trackData", function(result){
+        websitesToTrack = JSON.parse(result.trackData);
+    });
+});
+
+startUp();
+
+// Do all the startup tasks
+function startUp() {
+    //Initialize the totalTimeOnWebsites variable to the data gained from the local storage of the user
+    chrome.storage.local.get(null, function(result){
+        totalTimeOnWebsites = result["timeData" + numDaysSinceUTC()];
+        today = result["today"];
+        websitesToTrack = JSON.parse(result.trackData);
+    });
+    // Updating the ActiveTabUrl during initialization
+    updateActiveTabUrl();
+
+    // Register Events
+    registerEvents(); // This one is important better left untouched
+
+    // Setting isUserActive as true while starting up
+    isUserActive = true;
+
+    updateData();
+    // Setting up the listener that will check if a new day is there
+    setInterval(function(){
+    if(isNewDay()){
+        updateData();
+      }
+    }, 1000);
+
+}
+
+
+
+function updateData(){
+    totalTimeOnWebsites = 0;
+    var newStorageName = "timeData" + numDaysSinceUTC();
+    var newData = {};
+    newData[newStorageName] = totalTimeOnWebsites;
+    todayStorageName = newStorageName;
+    today = numDaysSinceUTC();
+    newData["today"] = numDaysSinceUTC();
+    chrome.storage.local.set(newData, function(){});
+}
+
+function registerEvents() {
+    // Registering for onActivated event
+    // This is fired when the active tab changes
+    chrome.tabs.onActivated.addListener(function(activeInfo) {
+        updateActiveTabUrl();
+    });
+
+    // Registering for onChanged event
+    // This is fired when the url of a tab changes
+    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+        updateActiveTabUrl();
+    });
+
+    // Registering for onFocusChanged event
+    // This is fired when the active chrome window is changed.
+    chrome.windows.onFocusChanged.addListener(function(windowId) {
+        // This happens if all the windows are out of focus
+        // Using this condition to infer that the user is inactive
+        if (windowId === chrome.windows.WINDOW_ID_NONE) {
+            isUserActive = false;
+        } else {
+            isUserActive = true;
+        }
+        updateActiveTabUrl();
+    });
+
+    // Registering for an interval of 1s
+    window.setInterval(intervalListener, 1000); // Keeps listening to the current events 
+}
+
+// Listener for the timer
+function intervalListener() {
+    updateTotalTime();
+}
+
+/*
+* Function : updateTotalTime()
+* -----------------------------
+* Updates the total time spent on the watched website(s).
+*/
+function updateTotalTime() {
+    var currDomain = getActiveWebsite();  
+    if(isUserActiveNow() && isWatchedWebsite(currDomain)){
+        totalTimeOnWebsites += 1; 
+    }
+}
+
+/*
+* Function : isWatchedWebsite(domain name of the website)
+* -------------------------------------------------------
+* I know its a really bad programming style right now but first I have to get 
+* it to work.
+* The function returns true if the current domain name is facebook or twitter and
+* false otherwise
+*/
+function isWatchedWebsite(domainName){
+    return ($.inArray(domainName, websitesToTrack) > -1);
+}
+
+// Returns the last time when the timeMap has been refresehed
+function getLastRefreshTime() {
+    return lastRefreshTimeStamp;
+}
+
+// Finds the current tab in the current window
+// Updates the activeTabUrl global variable
+function updateActiveTabUrl() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) { // The argument of the call back function is an array of tabs
+        if (tabs.length < 1) { // If there are no tabs in the window, how the fuck is that possible ? let us see
+            activeTabUrl = null;
+        } else {
+            activeTabUrl = tabs[0].url;
+        }
+    });
+}
+
+/*
+* Function  : extractDomain(url of the website)
+* Usage : currentDomain = extractDomain("http://www.google.com/gmail/")
+* ----------------------------------------------------------------------
+* Extracts the domain name of the website and returns it as a string.
+* E.g. extractDomain("http://www.google.com/gmail/") would return "google.com"
+*/
+function extractDomain(str) {
+    // Removing the protocol and www prefixes
+    var strList = str.split(":\/\/");
+    if (strList.length > 1) {
+        str = strList[1];
+    } else {
+        str = strList[0];
+    }
+    str = str.replace(/www\./g,'');
+    
+    // Extracting the domain name from full URL
+    var domainName = str.split('\/')[0];
+    return domainName;
+}
+
+
+/* 
+* Function : getTimeOnTwitter()
+* -------------------------------
+* Returns the amount of time in seconds spent on 
+* the website www.twitter.com and www.facebook.com
+*/
+
+function getTimeOnFbTwitter(){
+    return totalTimeOnWebsites;
+}
+
+
+/*
+* Function : numDaysSinceUTC()
+* var numDaysSinceUTC = currentDate();
+* ------------------------------------
+* Returns the number of days that have passed since 
+* January 1 1970, used in the process of checking 
+* days that have passed
+*/
+function numDaysSinceUTC(){
+    var NUM_MILI_IN_A_DAY = 86400000;
+    var today = new Date();
+    var utcMili = Date.UTC(today.getFullYear(), today.getMonth(), today.getDate()); // miliseconds since UTC
+    return (utcMili/ NUM_MILI_IN_A_DAY);
+}
+
+/*
+* Function : isNewDay()
+* -----------------------
+* Checks if a new day is dawned upon us, 
+*/
+
+function isNewDay(){
+    return (numDaysSinceUTC() - today >= 1);
+}
+
+function todayTimeData(){
+    return todayStorageName;
+}
+
+
+
+
+
---
+++ js/popup.js
@@ -0,0 +1,253 @@
+// Stores the width of each bar in pixels
+var barWidth = 15;
+// Stores the spacing between each line bar in pixels
+var barSpacing = 10;
+
+
+// Quotes about wastage of time
+var timeQuotes = ["when you kill time, remember that it has no resurrection", 
+    "the trouble is, you think you have time",
+    "time is what we want most, but what we use worst",
+    "wasting your time is the subtlest form of suicide"
+];
+
+$(document).ready(function(){
+
+    var backgroundPage = chrome.extension.getBackgroundPage();
+    while(backgroundPage === null){
+      backgroundPage = chrome.extension.getBackgroundPage();  
+    }
+
+    if(backgroundPage !== null && backgroundPage.isFirstRun) {
+        $('.settingsPanel').addClass('is-visible');
+        $('.tracksiteInput').addClass('inputEnabled');
+        $('.settingsContainer').append("<img src = \"images/button_OK.png\" class = \"done\">");
+    } else {
+        $('.done').remove();
+        $('.tracksiteInput').css({"border": "none"});
+        $('.tracksiteInput').prop("disabled", true);
+        $('.tracksiteInput').addClass('inputDisabled');
+        $('.settingsContainer').append("<img src = \"images/button_EDIT.png\" class = \"editBtn\">");
+    }
+
+    $('.editBtn').on('click', function(){
+        $('.tracksiteInput').prop("disabled", false);
+        $('.tracksiteInput').addClass("inputEnabled");
+        $('.editBtn').remove();
+        $('.settingsContainer').append("<img src = \"images/button_OK.png\" class = \"done\">");
+    });
+
+    // Try to update the data from the localStorage if we can about the sites that are being tracked
+    if(chrome.extension.getBackgroundPage().isFirstRun){
+        chrome.storage.local.get("trackData", function(result){
+               var sitesBeingTracked = JSON.parse(result.trackData);
+               document.getElementById("firstSite").value = sitesBeingTracked[0];
+               document.getElementById("secondSite").value = sitesBeingTracked[1];
+               document.getElementById("thirdSite").value = sitesBeingTracked[2];
+               document.getElementById("fourthSite").value = (sitesBeingTracked[3] === undefined) ? "" : sitesBeingTracked[3];
+               document.getElementById("fifthSite").value = (sitesBeingTracked[4] === undefined) ? "" : sitesBeingTracked[4];
+        });
+    } else {
+        chrome.storage.local.get("trackData", function(result){
+            var sitesBeingTracked = JSON.parse(result.trackData);
+            var inputFieldIds = ["firstSite", "secondSite", "thirdSite", "fourthSite", "fifthSite"];
+            var inputIdPos = 0;
+            for(var i = 0 ; i < 5 ;i++){
+                if(sitesBeingTracked[i] != "") {
+                    document.getElementById(inputFieldIds[inputIdPos]).value = sitesBeingTracked[i];
+                    inputIdPos++;
+                }
+            }
+        });
+    }
+
+    displayFacebookAndTwitterTime();
+
+    // Updates the visible time in real time 
+    setInterval(function(){
+         displayFacebookAndTwitterTime(); 
+     }, 1000);
+
+    // For the settings panel to become visible
+    $('.settings').on('click', function(event){
+        $('.settingsPanel').addClass('is-visible');
+    });
+
+    $('.settingsPanel').on('click', function(event){
+        // This function registers click on the side panel
+        if($(event.target).is('.done')) { 
+            $('.settingsPanel').removeClass('is-visible');
+            $('.editBtn').remove();
+
+            var firstSiteBeingTracked = document.getElementById("firstSite").value;
+            var secondSiteBeingTracked = document.getElementById("secondSite").value;
+            var thirdSiteBeingTracked = document.getElementById("thirdSite").value;
+            var fourthSiteBeingTracked = document.getElementById("fourthSite").value;
+            var fifthSiteBeingTracked = document.getElementById("fifthSite").value;
+
+            // Now whoever of them are not null we will need to push there value into the array
+            var sitesBeingTracked = [firstSiteBeingTracked, secondSiteBeingTracked, thirdSiteBeingTracked, fourthSiteBeingTracked, fifthSiteBeingTracked];
+            var sitesBeingTrackedStorable = JSON.stringify(sitesBeingTracked);
+            chrome.storage.local.set({"trackData" : sitesBeingTrackedStorable}, function(){});
+            chrome.extension.getBackgroundPage().isFirstRun = false;
+            $('.tracksiteInput').removeClass('inputEnabled');
+            $('.tracksiteInput').addClass('inputDisabled');
+            $('.tracksiteInput').prop("disabled", true);
+            $('.done').remove();
+            $('.settingsContainer').append("<img src = \"images/button_EDIT.png\" class = \"editBtn\">").on('click', function(){
+                $('.tracksiteInput').prop("disabled", false);
+                $('.tracksiteInput').addClass("inputEnabled");
+                $('.done').remove();
+                $('.editBtn').remove();
+                $('.settingsContainer').append("<img src = \"images/button_OK.png\" class = \"done\">");
+            });
+
+        }
+
+        if($(event.target).is('.settingsPanel') && !chrome.extension.getBackgroundPage().isFirstRun) {
+            $('.settingsPanel').removeClass('is-visible');
+        }
+    });
+
+    // Reposition the elements on the page
+    var windowHeight = $(window).height();
+    $(".container").css({
+        "height" : windowHeight * 0.36,
+        "margin-top" : windowHeight * 0.25
+    });
+
+
+    // Update the page with a random quote on each reload
+    if(chrome.extension.getBackgroundPage().totalTimeOnWebsites > 0){
+        var randomQuote = timeQuotes[Math.floor(Math.random() * timeQuotes.length)];
+        var trackerDisplay = document.getElementById("websites");
+        trackerDisplay.innerHTML = randomQuote;
+    }else{
+        var trackerDisplay = document.getElementById("websites");
+        trackerDisplay.innerHTML = "going great!";
+    }
+
+
+
+
+    var forLabels = [];
+    var forValues = [];
+    // Get the data from the chrome local storage thing
+    chrome.storage.local.get(null, function(extDatas){
+        for(var prop in extDatas){
+            if(prop.lastIndexOf("timeData", 0) === 0){
+                forLabels.push(prop);
+                forValues.push(extDatas[prop]);
+            }
+        }
+
+        console.log(forValues.toString());
+
+        var barChartData = {
+                labels : [].concat(forLabels),
+                datasets : [
+                    {
+                        fillColor : "rgba(220,220,220,0.6)",
+                        data : [].concat(forValues)
+                    }
+                ],
+
+                getNumData : function(){
+                    return Number(this.labels.length);
+                }
+        }
+
+        var chartView = document.getElementById("myChart");
+        var ctx = chartView.getContext("2d");
+        window.myBar = new Chart(ctx).Bar(barChartData, {
+            responsive : false,
+            barShowStroke : false,
+            scaleShowGridLines : false,
+            showScale : false,
+            showTooltips :false,
+            barValueSpacing  : 13
+        });
+
+        var totalWidthBars = barWidth * barChartData.getNumData();
+        var totalWidthSpacing = barSpacing * (barChartData.getNumData() - 1);
+        var totalWidthChart = totalWidthBars + totalWidthSpacing;
+        var left = ($(window).width() - totalWidthChart)/2;
+
+        $("#myChart").css({
+            "width": totalWidthChart,
+            "height" : "50%",
+            "left" : left
+        });
+    });
+
+
+});
+
+$(window).resize(function(){
+    var totalWidthChart = $("#myChart").width();
+    var left = ($(window).width() - totalWidthChart)/2;
+
+    var windowHeight = $(window).height();
+    var windowWidth  = $(window).width();
+
+    $("#myChart").css({
+        "width": totalWidthChart,
+        "height" :  windowHeight * 0.5,
+        "left" : left
+    });
+
+    $(".container").css({
+        "height" : windowHeight * 0.36,
+        "margin-top" : windowHeight * 0.25
+    });
+});
+
+/*
+* Function : displayFacebookAndTwitterTime();
+* -------------------------------------------
+* This function has a little name problem the thing is that this function
+* shows the time for facebook twitter and google plus but due to 
+* evolution over the times the name has stuck
+*/
+function displayFacebookAndTwitterTime() {
+    var backgroundPage = chrome.extension.getBackgroundPage();
+    if(backgroundPage != null){
+        var totalTimeSpent = chrome.extension.getBackgroundPage().totalTimeOnWebsites;
+        var div = document.getElementById("actualTime");
+        div.innerHTML = getReadableTime(totalTimeSpent);
+        var storageName = "timeData" + chrome.extension.getBackgroundPage().numDaysSinceUTC();
+        var dataToBeWritten = {};
+        dataToBeWritten[storageName] = totalTimeSpent;
+        dataToBeWritten["today"] = chrome.extension.getBackgroundPage().numDaysSinceUTC();
+        chrome.storage.local.set(dataToBeWritten, function(){});
+    }
+}
+
+/*
+* Function : getReadableTime();
+* Usage : var readableTime = getReadableTime(2000);
+* --------------------------------------------------
+* This function takes in the number of seconds as arguments and then
+* converts those number of seconds into hours and minutes respectively
+* and returns a string that holds the proper time in the format of '3 hours 45 minutes and 3 seconds'
+*/
+function getReadableTime(totalSeconds) {
+    var seconds = totalSeconds % 60;
+    var minutes = (Math.floor(totalSeconds/60))%60;
+    var hours = (Math.floor(totalSeconds/3600));
+    var readableTime = '';
+    if (hours > 1) 
+        readableTime += hours + ' hours ';
+    else if(hours == 1)
+        readableTime += hours + ' hour '
+    if (minutes > 1)
+        readableTime += minutes + ' minutes and ' ;
+    else if(minutes == 1)
+        readableTime += minutes + ' minute and '
+    if(seconds == 1){
+        readableTime += seconds + ' second ';
+    }else{
+        readableTime += seconds + ' seconds ';
+    }
+    return readableTime;
+}
